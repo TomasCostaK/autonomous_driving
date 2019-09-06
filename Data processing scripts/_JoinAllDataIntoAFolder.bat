b1004a808ce734b92f8b54e44fa670a5ec275157
Diogo Guedes <diogoguedes@ua.pt>
2019-09-06 14:31:55 +0100

    Removed the Samples directory and added a batch script that colorizes and moves all the colored point clouds (taken from different positions) into a single target directory

---
+++ Data processing scripts/_JoinAllDataIntoAFolder.bat
@@ -0,0 +1,62 @@
+REM Rather strangely will send echo off to the output! 
+REM silently turns off command echoing, and only output the batch author intended to be written is actually written.
+@ECHO OFF
+
+REM You always have to use SETLOCAL ENABLEDELAYEDEXPANSION and !...! instead of %...% when working with variables which are modified inside a loop.
+setlocal enabledelayedexpansion
+
+REM create the directory that will hold all colored point clouds
+if not exist ".\_EveryPointCloud\" ( 
+	mkdir .\_EveryPointCloud
+)
+
+REM counts the number of directories in the current directory
+for /f %%a in ('dir /b /ad %folder%^|find /c /v "" ') do (
+	set count=%%a
+)
+REM create variable which holds the number of directories that hold point cloud data
+REM ignores the directory created at the beginning of this program
+set /A dirPointCloudCounter = %count%-1
+
+if not exist ".\LiDAR_PointCloud\" (
+	dirPointCloudCounter = %count%
+)
+
+REM create string variable
+set dirPointCloudBaseName=LiDAR_PointCloud
+
+set cloudPointTxtFileName=LiDAR_PointCloud_points.txt
+set destDirNameForGeneratedPlyFiles=.\_EveryPointCloud\
+
+set baseCloudPointFileName=LiDAR_PointCloud.ply
+set baseErrorCloudPointFileName=LiDAR_PointCloud_error.ply
+
+REM Starts at 1, steps by one, and finishes at dirPointCloudCounter.
+REM %%x if inside batch file; if in console use %x
+for /l %%x in (1, 1, %dirPointCloudCounter%) do ( 
+	SET "dirNum=%%x"
+    SET dirName=%dirPointCloudBaseName%!dirNum!
+    ECHO ---- Processing data in the !dirName! directory
+	
+	REM if the directory exsits
+	if exist ".\!dirName!\" ( 
+		echo ".\!dirName!\%cloudPointTxtFileName%"
+		
+		REM create the colored point clouds
+		python colorize.py ".\!dirName!\%cloudPointTxtFileName%"
+		
+		REM after using the base point cloud file (LiDAR_PointCloud.ply), delete it
+		del ".\!dirName!\%baseCloudPointFileName%" 
+		del ".\!dirName!\%baseErrorCloudPointFileName%" 
+		del ".\!dirName!\*.txt" 
+		del ".\!dirName!\*.bmp" 
+		
+		REM add a number at the end of each .ply file (before the extension) and copy them to the destination folder
+		copy /y .\!dirName!\*.ply !destDirNameForGeneratedPlyFiles!*!dirNum!.ply
+		
+		REM delete the leftover ply files
+		del ".\!dirName!\*.ply" 
+		
+		rmdir .\!dirName!
+	)
+)
