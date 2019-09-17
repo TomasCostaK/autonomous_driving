42c1b8365e6993f767339227d77570f636c601de
Diogo Guedes <diogoguedes@ua.pt>
2019-09-17 18:25:20 +0100

    Make one script that supports both uncolored and colored point clouds for label painting

--- Data processing scripts/ColorizePointCloudsByLabels.py
+++ Data processing scripts/ColorizePointCloudsByLabels.py
@@ -1,23 +1,57 @@
-import random
+#
+# This script colors point clouds according to the label assigned to each point.
+# Supports uncolored and colored point clouds as input.
+# Each unique label is assigned a random rgb color before adding it to the appropriate points.
+#
 
+import random
+import sys
+
+
+# from an array of repeated values, return a new array which holds all the unique values in it
 def getAllUniqueValuesFromList(seq):
 	b = []
 	for i in seq:
 		if i not in b:
 			b.append(i)
 	return b
+	
+arg1 = sys.argv[1]	# path to the the point cloud file (.ply) to be colored
+arg2 = sys.argv[2]  # path to the text file that holds the label information
+arg4 = sys.argv[3]	# 0 means the original point cloud file is uncolored;
+					# 1 means the original point cloud file is colored;
 
-pointCloudFilenameRecolored = 'LiDAR_PointCloud_points_PC_Day1_Recolored.ply'
-pointCloudFilename = 'LiDAR_PointCloud_points_PC_Day1.ply'
-numLinesInPlyHeader = 10 
-labelsFilename = 'LiDAR_PointCloud_labels1.txt'
+# create a name for the generated label colored point cloud file
+pointCloudFilenameRecolored = arg1.split('.')[0] + '_recolored' + '.ply'
+pointCloudFilename = arg1
+labelsFilename = arg2
 
-# store every line in the point cloud file in a list o strings (without 7n character at the end of each line)
+numLinesInPlyHeader = -1
+
+if int(arg4) == 0:				# the original point cloud is uncolored
+	numLinesInPlyHeader = 7
+elif int(arg4) == 1:			# the original point cloud is colored
+	numLinesInPlyHeader = 10
+else:
+	print('The 3rd argument has to have a value of:\n\t - 0: if the original point cloud is uncolored\n\t - 1: if the original point cloud is colored')
+	sys.exit()
+
+# store every line of the point cloud file in a list of strings (without \n character at the end of each line)
 # The first 10 elements correspond to the header of a .ply file
 pointCloudLines = [line.rstrip('\n') for line in open(pointCloudFilename)]
 
-plyHeaderLines = pointCloudLines[:numLinesInPlyHeader]
-print(plyHeaderLines)
+plyHeaderLines = []
+
+if numLinesInPlyHeader == 7:
+	plyHeaderLines.extend(pointCloudLines[:numLinesInPlyHeader-1]) # do not include the end header for supporting only uncolored point cloud
+	plyHeaderLines.extend(['property uchar red'])
+	plyHeaderLines.extend(['property uchar green'])
+	plyHeaderLines.extend(['property uchar blue'])
+	plyHeaderLines.extend(['end_header'])
+elif numLinesInPlyHeader == 10:
+	plyHeaderLines.extend(pointCloudLines[:numLinesInPlyHeader])
+
+
 # remove the header elements/lines
 pointCloudLines = pointCloudLines[numLinesInPlyHeader:]
 
@@ -64,10 +98,9 @@ for i in range(len(pointCloudLines)):
 	rgbTuple = colorIdDictionary.get(pointId)
 	
 	newPointCloudLines.append(lineList[0] + ' ' + lineList[1] + ' ' + lineList[2] + ' ' + str(rgbTuple[0]) + ' ' + str(rgbTuple[1]) + ' ' + str(rgbTuple[2]))
-	#print(pointCloudLines[i])
 
 
-with open(pointCloudFilenameRecolored, mode="w") as outfile:  # also, tried mode="rb"
+with open(pointCloudFilenameRecolored, mode="w") as outfile: 
 	# header
 	for i in range(0, len(plyHeaderLines)):
 		line = plyHeaderLines[i] + '\n'
@@ -75,6 +108,7 @@ with open(pointCloudFilenameRecolored, mode="w") as outfile:  # also, tried mode
 	
 	# points
 	for i in range(0, len(newPointCloudLines)):
+		
 		line = newPointCloudLines[i] + '\n'
 		outfile.write(line)      
 
